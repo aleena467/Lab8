b812dcc9dcf2ff4538fcf188be7f0b4674b26f3f
aleena467 <achaudh2@ualberta.ca>
2024-10-25 08:27:47 -0600

    Add addCity method and unit test for CustomList

---
+++ app/src/test/java/com/example/lab8/CustomList.java
@@ -0,0 +1,27 @@
+package com.example.lab8;
+
+import java.util.ArrayList;
+
+public class CustomList {
+    private ArrayList<City> cities;
+
+    public CustomList() {
+        this.cities = new ArrayList<>();
+    }
+
+    /**
+     * this gets size of the list
+     * @return the size of the list
+     */
+    public int getCount() {
+        return cities.size();
+    }
+
+    /**
+     * this adds a city object to the list
+     * @param city the city to add
+     */
+    public void addCity(City city) {
+        cities.add(city);
+    }
+}
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,32 @@
 package com.example.lab8;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
+    /**
+     * create a mock list for testing
+     * @return a new CustomList instance
+     */
+    @BeforeEach
+    public void setUp() {
+        list = new CustomList();
+    }
 
+    /**
+     * test the addCity method
+     * verifies that the list size increases by one when a city is added
+     */
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
